f69fb8bd9004a2cde7f27d81df806c19aa673a5c
abdvlla <abood67200@gmail.com>
2024-06-06 18:21:21 -0300

    no api key

--- movie-rating-app/src/pages/home/query.ts
+++ movie-rating-app/src/pages/home/query.ts
@@ -1,10 +1,11 @@
+const apiKey = import.meta.env.VITE_API_KEY;
 
 export const fetchMovies = async () => {
     const res = await fetch(
       "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
       {
         headers: {
-          Authorization: `Bearer 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzA0NzQ0MmE2Y2JmN2VlY2Q1YTE3MTIxYTFjMWViZSIsInN1YiI6IjY0MWRlMTFhYmVmYjA5MDA4OGJlN2Y2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CGSURAve9aQqMsg3cCJBv-FgkLQqZJiDm5bgv_mjBOc'`
+          Authorization: `Bearer ${apiKey}`
         }
       }
     );
@@ -21,7 +22,7 @@ export const fetchMovies = async () => {
       "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1",
       {
         headers: {
-          Authorization: `Bearer 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzA0NzQ0MmE2Y2JmN2VlY2Q1YTE3MTIxYTFjMWViZSIsInN1YiI6IjY0MWRlMTFhYmVmYjA5MDA4OGJlN2Y2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CGSURAve9aQqMsg3cCJBv-FgkLQqZJiDm5bgv_mjBOc'`
+          Authorization: `Bearer ${apiKey}`
         }
       }
     );
--- movie-rating-app/src/pages/movie/query.ts
+++ movie-rating-app/src/pages/movie/query.ts
@@ -1,11 +1,11 @@
-
+const apiKey = import.meta.env.VITE_API_KEY;
 
 export const fetchMovieDetails = async (movieId: string) => {
     const res = await fetch(
       `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
       {
         headers: {
-          Authorization: `Bearer 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YzA0NzQ0MmE2Y2JmN2VlY2Q1YTE3MTIxYTFjMWViZSIsInN1YiI6IjY0MWRlMTFhYmVmYjA5MDA4OGJlN2Y2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CGSURAve9aQqMsg3cCJBv-FgkLQqZJiDm5bgv_mjBOc'}`
+          Authorization: `Bearer ${apiKey}`
         }
       }
     );
